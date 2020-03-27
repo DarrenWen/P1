cae040145448489c07de348b5ec7392dfd20060b
Wiggins <380891124@qq.com>
2020-03-27 20:10:02 +0800

    111

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-using System;
+ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -12,7 +12,7 @@ namespace P1
     public class Program
     {
         public static void Main(string[] args)
-        {
+        { 
             CreateHostBuilder(args).Build().Run();
         }
 
